3c538bf15165894d81bb44e2261c4e94c4508f86
Niklaus Hirt <niklaushirt@gmail.com>
2019-05-14 15:13:29 +0200

    new

---
+++ helm/charts/build_private.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#-----------------------------------------------------------------------------------------------------------------
+#REMOVE PACKAGES
+#-----------------------------------------------------------------------------------------------------------------
+rm ./*.tgz
+
+#REMOVE INDEX.YAML
+rm ./index.yaml
+
+#-----------------------------------------------------------------------------------------------------------------
+#CREATE PACKAGES
+#-----------------------------------------------------------------------------------------------------------------
+helm package guestbook
+
+
+#HACK for DEMO LIBERTY
+mv *.tgz  ../repo/stable/
+
+
+#CREATE INDEX.YAML
+cd ../repo/stable
+
+helm repo index --url https://raw.githubusercontent.com/niklaushirt/guestbook/master/helm/repo/stable/ ./
+
+cd ../../charts
